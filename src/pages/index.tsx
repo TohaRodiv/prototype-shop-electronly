697d1c5029d3ee09bbbe5fbfd64f47abf347c18e
TohaRodiv <toha.rodiv@yandex.ru>
2021-01-30 14:14:37 +0300

    fix indents

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,45 +6,45 @@ import { Section } from "@component/Section";
 import { StockBanner } from "@component/StockBanner";
 import { Container } from "react-grid-system";
 
- 
+
 const cats = [
-	{id: 1, path: "/", image: "/images/categories/iphones.jpg", title: "Iphones"},
-	{id: 2, path: "/", image: "/images/categories/photos.jpg", title: "Photos"},
-	{id: 3, path: "/", image: "/images/categories/tv.jpg", title: "4K TV"},
-	{id: 4, path: "/", image: "/images/categories/smart-watch.jpg", title: "Smart watch"},
-	{id: 5, path: "/", image: "/images/categories/iphones.jpg", title: "Iphones"},
-	{id: 6, path: "/", image: "/images/categories/photos.jpg", title: "Photos"},
-	{id: 7, path: "/", image: "/images/categories/tv.jpg", title: "4K TV"},
+	{ id: 1, path: "/", image: "/images/categories/iphones.jpg", title: "Iphones" },
+	{ id: 2, path: "/", image: "/images/categories/photos.jpg", title: "Photos" },
+	{ id: 3, path: "/", image: "/images/categories/tv.jpg", title: "4K TV" },
+	{ id: 4, path: "/", image: "/images/categories/smart-watch.jpg", title: "Smart watch" },
+	{ id: 5, path: "/", image: "/images/categories/iphones.jpg", title: "Iphones" },
+	{ id: 6, path: "/", image: "/images/categories/photos.jpg", title: "Photos" },
+	{ id: 7, path: "/", image: "/images/categories/tv.jpg", title: "4K TV" },
 ];
 
 const products = [
-	{id: 1, path: "/", image: "/images/categories/iphones.jpg", title: "Iphones", price: 1000},
-	{id: 2, path: "/", image: "/images/categories/photos.jpg", title: "Photos", price: 1000},
-	{id: 3, path: "/", image: "/images/categories/tv.jpg", title: "4K TV", price: 1000},
-	{id: 4, path: "/", image: "/images/categories/iphones.jpg", title: "Iphones", price: 1000},
-	{id: 5, path: "/", image: "/images/categories/photos.jpg", title: "Photos", price: 1000},
-	{id: 6, path: "/", image: "/images/categories/tv.jpg", title: "4K TV", price: 1000},
-	{id: 7, path: "/", image: "/images/categories/smart-watch.jpg", title: "Smart watch"},
+	{ id: 1, path: "/", image: "/images/categories/iphones.jpg", title: "Iphones", price: 1000 },
+	{ id: 2, path: "/", image: "/images/categories/photos.jpg", title: "Photos", price: 1000 },
+	{ id: 3, path: "/", image: "/images/categories/tv.jpg", title: "4K TV", price: 1000 },
+	{ id: 4, path: "/", image: "/images/categories/iphones.jpg", title: "Iphones", price: 1000 },
+	{ id: 5, path: "/", image: "/images/categories/photos.jpg", title: "Photos", price: 1000 },
+	{ id: 6, path: "/", image: "/images/categories/tv.jpg", title: "4K TV", price: 1000 },
+	{ id: 7, path: "/", image: "/images/categories/smart-watch.jpg", title: "Smart watch" },
 ];
 
 const promos = [
-	{id: 1, title: "Welcome", image: "/images/banners/welcome.jpg"},
-	{id: 2, title: "Work hard", image: "/images/banners/work.jpg"},
-	{id: 3, title: "Open", image: "/images/banners/open.jpg"},
+	{ id: 1, title: "Welcome", image: "/images/banners/welcome.jpg" },
+	{ id: 2, title: "Work hard", image: "/images/banners/work.jpg" },
+	{ id: 3, title: "Open", image: "/images/banners/open.jpg" },
 ];
 
 
 const Home: React.FC = (): JSX.Element => (
 	<MainLayout title="Главаная | Electronly" noContainer noPaddingTop>
 		<Section>
-			<StockBanner promotions={ promos }></StockBanner>
+			<StockBanner promotions={promos}></StockBanner>
 		</Section>
 		<Container>
 			<Section sm>
 				<CategoryList categories={cats} />
 			</Section>
 			<Section sm>
-				<ProductList gridType="grid" products={ products } />
+				<ProductList gridType="grid" products={products} />
 			</Section>
 			<Section sm>
 				<Typography variant="h2">О нас</Typography>
